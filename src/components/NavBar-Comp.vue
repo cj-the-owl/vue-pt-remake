fc2089d7a0d8edefc311044e0665d4af42d21564
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-06-21 16:37:01 +0200

    first

---
+++ src/components/NavBar-Comp.vue
@@ -0,0 +1,30 @@
+<!-- template is where the html goes -->
+<template> 
+    <nav>
+        <img src="https://img.logoipsum.com/265.svg" alt="logo">
+        <!-- div for nav to home about and properties -->
+        <!-- to= is to direct where like an href in a normal navabar -->
+        <div>
+            <!-- router-link is like an a tag -->
+            <router-link to="/">Home</router-link> ||
+            <router-link to="/about">About</router-link> ||
+            <router-link to="/properties">Properties</router-link>
+        </div>
+
+        <!-- div for login and register nav -->
+        <!-- v-if is to show if a someone that isnt a user is viewing -->
+        <div v-if="!user">
+            <router-link to="/login">Login</router-link> ||
+            <router-link to="/register">Register</router-link>
+        </div>
+    </nav>
+</template>
+<script>
+    export default {
+        computed: {
+            user() {
+                return this.$store.state.user
+            }
+        }
+    }
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    properties: null,
+    property: null,
+    user: null,
+    asc: true,
   },
-  getters: {
-  },
+  
   mutations: {
+    setUser: (state, user) => {
+      state.user = user;
+    },
+    setProperties: (state, properties) => {
+      state.properties = properties;
+    },
+    setProperty: (state, property) => {
+      state.property = property;
+    },
+    sortPropertiesByPrice: (state) => {
+      state.properties.sort((a, b))
+    }
   },
   actions: {
   },
-  modules: {
-  }
+  
 })
